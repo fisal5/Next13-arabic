e160a2c0742a2643e9b394f2846a6679fe075c52
fisal <fisaljabbarin@gmail.com>
2024-01-24 17:24:30 +0100

    8questions

---
+++ app/components/SoundsComponent/eagleSound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SoundButton from '../soundButton';
+
+const eagleSound = () => {
+  return (
+    <div>
+      <SoundButton audioSrc="/Eagle.mp3" />
+    </div>
+  )
+};
+
+export default eagleSound;
--- app/quiz/data.js
+++ app/quiz/data.js
@@ -9,9 +9,17 @@ import GiraffeSound from "../components/SoundsComponent/giraffeSound";
 import BearSound from "../components/SoundsComponent/bearSound";
 import DogImage from "../components/ImagesComponent.jsx/DogImage";
 import DogSound from "../components/SoundsComponent/dogSound";
+import BirdSound from "../components/SoundsComponent/birdSound";
+import BirdImage from "../components/ImagesComponent.jsx/BirdImage";
+import TigerImage from "../components/ImagesComponent.jsx/TigerImage";
+import MonkeyImage from "../components/ImagesComponent.jsx/MonkeyImage";
+import CatImage from "../components/ImagesComponent.jsx/CatImage";
+import TigerSound from "../components/SoundsComponent/tigerSound";
+import CatSound from "../components/SoundsComponent/catSound";
+import MonkeySound from "../components/SoundsComponent/monkeySound";
 
 export const quiz = {
-  totalQuestions: 4,
+  totalQuestions: 5,
   questions: [
     {
       id: 1,
@@ -57,6 +65,51 @@ export const quiz = {
       ],
       correctAnswer: { id: 3, value: (<DogImage />) },
     },
+    {
+      id: 5,
+      question: (<BirdSound />),
+      answers: [
+        { id: 1, value: (<MonkeyImage />) },
+        { id: 2, value: (<BirdImage />) },
+        { id: 3, value: (<TigerImage />) },
+        { id: 4, value: (<RabbitImage />) },
+      ],
+      correctAnswer: { id: 2, value: (<BirdImage />) },
+    },
+    {
+      id: 6,
+      question: (<TigerSound />),
+      answers: [
+        { id: 1, value: (<LionsImage />) },
+        { id: 2, value: (<BirdImage />) },
+        { id: 3, value: (<TigerImage />) },
+        { id: 4, value: (<CatImage />) },
+      ],
+      correctAnswer: { id: 3, value: (<TigerImage />) },
+    },
+    {
+      id: 7,
+      question: (<CatSound />),
+      answers: [
+        { id: 1, value: (<GirrafeImage />) },
+        { id: 2, value: (<BearImage />) },
+        { id: 3, value: (<RabbitImage />) },
+        { id: 4, value: (<CatImage />) },
+      ],
+      correctAnswer: { id: 4, value: (<CatImage />) },
+    },
+    {
+      id: 8,
+      question: (<MonkeySound />),
+      answers: [
+        { id: 1, value: (<ElephantImage />) },
+        { id: 2, value: (<EagleImage />) },
+        { id: 3, value: (<MonkeyImage />) },
+        { id: 4, value: (<TigerImage />) },
+      ],
+      correctAnswer: { id: 3, value: (<MonkeyImage />) },
+    },
+    
     
   ],
 };
