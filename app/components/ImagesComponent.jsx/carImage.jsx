11940deb52eb1d25fc56176025cfb535a80b5de8
fisal <fisaljabbarin@gmail.com>
2024-04-08 12:43:16 +0200

    transport

---
+++ app/components/ImagesComponent.jsx/carImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ImageComponent from '../ImageComponent'
+ 
+
+const CarImage = () => {
+  return (
+    <div>
+      <ImageComponent src="/car.webp" alt="Webp" />
+    </div>
+  )
+}
+
+export default CarImage;
