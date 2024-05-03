624955186fd257ee1ecacd2aa997b105840735a5
fisal <fisaljabbarin@gmail.com>
2024-05-03 06:49:13 +0200

    verbs

---
+++ app/components/ImagesComponent.jsx/pullImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ImageComponent from '../ImageComponent'
+ 
+
+const PullImage = () => {
+  return (
+    <div>
+      <ImageComponent src="/pull.webp" alt="Pull" />
+    </div>
+  )
+}
+
+export default PullImage;
--- app/page.js
+++ app/page.js
@@ -100,6 +100,19 @@ export default function Home() {
           </div>
         </Link>
       </div>
+      <div>
+        <Link href='verbsquiz'>
+          <div className="crusor-pointer inline-block p-2 px-5 bg-blue-500 text-white rounded-lg text-center">
+            <Image className="w-64 h-52"
+              src= '/verbs.webp'
+              alt="verbs image"
+              priority={true}
+              width={768}
+              height={300}
+            /> 
+          </div>
+        </Link>
+      </div>
     </div>
       
          
