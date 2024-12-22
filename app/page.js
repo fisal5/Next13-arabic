35392454dea85b20d35bb2297e8e38b33a237899
fisal <fisaljabbarin@gmail.com>
2024-12-22 01:17:23 +0100

    koko

--- app/page.js
+++ app/page.js
@@ -139,6 +139,19 @@ export default function Home() {
           </div>
         </Link>
       </div>
+      <div>
+        <Link href='bodyquiz'>
+          <div className="crusor-pointer inline-block p-2 px-5 bg-blue-500 text-white rounded-lg text-center">
+            <Image className="w-64 h-52"
+              src= '/body.jpeg'
+              alt="body image"
+              priority={true}
+              width={768}
+              height={300}
+            /> 
+          </div>
+        </Link>
+      </div>
     </div>
     
     
